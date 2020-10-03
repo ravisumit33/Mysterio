4bda7b5cc5b529bd325ba8b1014edc02e66bf42a
Nilesh Vaishnav <nilesh.iitg@gmail.com>
2020-10-03 19:21:48 +0530

    Added material-ui and material-fonts and cleaned create-react-app

---
+++ frontend/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <CssBaseline>
+        <Box className="App">
+          Work in progress
+        </Box>
+      </CssBaseline>
+    );
+  }
+}
+
+export default App;

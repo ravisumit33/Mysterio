4c0baff58c8a8284e9c7faf92a7715d2e8063458
Sumit Kumar <ravi.sumit33@gmail.com>
2020-11-28 03:34:12 +0530

    Modified store variable

--- frontend/src/components/Avatar/index.js
+++ frontend/src/components/Avatar/index.js
@@ -14,14 +14,12 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const ImageAvatar = (props) => {
-  const { chatWindowStore, className } = props;
-  return (
-    <Avatar className={className} alt={chatWindowStore.name} src={chatWindowStore.avatarUrl} />
-  );
+  const { store, className } = props;
+  return <Avatar className={className} alt={store.name} src={store.avatarUrl} />;
 };
 
 ImageAvatar.propTypes = {
-  chatWindowStore: PropTypes.shape({
+  store: PropTypes.shape({
     name: PropTypes.string.isRequired,
     avatarUrl: PropTypes.string.isRequired,
   }).isRequired,
@@ -33,21 +31,18 @@ ImageAvatar.defaultProps = {
 };
 
 const TextAvatar = (props) => {
-  const { chatWindowStore, className } = props;
+  const { store, className } = props;
   return (
     <Avatar
-      className={clsx(
-        useStyles({ avatarBg: generateRandomColor(chatWindowStore.name) }).avatar,
-        className
-      )}
+      className={clsx(useStyles({ avatarBg: generateRandomColor(store.name) }).avatar, className)}
     >
-      {chatWindowStore.name.charAt(0).toUpperCase()}
+      {store.name.charAt(0).toUpperCase()}
     </Avatar>
   );
 };
 
 TextAvatar.propTypes = {
-  chatWindowStore: PropTypes.shape({
+  store: PropTypes.shape({
     name: PropTypes.string.isRequired,
   }).isRequired,
   className: PropTypes.string,
@@ -58,17 +53,17 @@ TextAvatar.defaultProps = {
 };
 
 const CustomAvatar = (props) => {
-  const { chatWindowStore, className } = props;
+  const { store, className } = props;
 
-  return chatWindowStore.avatarUrl ? (
-    <ImageAvatar className={className} chatWindowStore={chatWindowStore} />
+  return store.avatarUrl ? (
+    <ImageAvatar className={className} store={store} />
   ) : (
-    <TextAvatar className={className} chatWindowStore={chatWindowStore} />
+    <TextAvatar className={className} store={store} />
   );
 };
 
 CustomAvatar.propTypes = {
-  chatWindowStore: PropTypes.shape({
+  store: PropTypes.shape({
     name: PropTypes.string.isRequired,
     avatarUrl: PropTypes.string,
   }).isRequired,

12f6fd7dedfbadf270a6b606792b8403d79d01d6
Sumit Kumar <ravi.sumit33@gmail.com>
2021-05-19 12:24:17 +0530

    Fixing consumer disconnect for individual channel

--- chat/consumers.py
+++ chat/consumers.py
@@ -65,25 +65,25 @@ class ChatConsumer(WebsocketConsumer):
     def disconnect(self, code):
         if self.channel_id is None:
             return
-        if self.room_id is not None:
-            if not self.is_group_consumer:
-                group_prefix = PREFIX.INDIVIDUAL_ROOM
-                Channel.IndividualChannel.objects.filter(pk=self.channel_id).delete()
-                async_to_sync(self.channel_layer.group_discard)(
-                    PREFIX.INDIVIDUAL_CHANNEL + str(self.channel_id), self.channel_name
+        if not self.is_group_consumer:
+            group_prefix = PREFIX.INDIVIDUAL_ROOM
+            Channel.IndividualChannel.objects.filter(pk=self.channel_id).delete()
+            async_to_sync(self.channel_layer.group_discard)(
+                PREFIX.INDIVIDUAL_CHANNEL + str(self.channel_id), self.channel_name
+            )
+            logger.info("Individual channel deleted")
+        else:
+            group_prefix = PREFIX.GROUP_ROOM
+            try:
+                Message.TextMessage.objects.create(
+                    group_room_id=self.room_id,
+                    sender_channel_id=self.channel_id,
+                    text=f"{self.profile['name']} left",
+                    message_type=MESSAGE.USER_LEFT,
                 )
-                logger.info("Individual channel deleted")
-            else:
-                group_prefix = PREFIX.GROUP_ROOM
-                try:
-                    Message.TextMessage.objects.create(
-                        group_room_id=self.room_id,
-                        sender_channel_id=self.channel_id,
-                        text=f"{self.profile['name']} left",
-                        message_type=MESSAGE.USER_LEFT,
-                    )
-                except IntegrityError:
-                    return
+            except IntegrityError:
+                return
+        if self.room_id is not None:
             async_to_sync(self.channel_layer.group_discard)(
                 group_prefix + str(self.room_id), self.channel_name
             )

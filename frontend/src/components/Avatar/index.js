28a4044a4856f1b1a05f361291d269a1b3df3aa5
Sumit Kumar <ravi.sumit33@gmail.com>
2021-07-06 12:31:56 +0530

    Remove context use from avatar

--- frontend/src/components/Avatar/index.js
+++ frontend/src/components/Avatar/index.js
@@ -1,16 +1,14 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Avatar, Icon, makeStyles } from '@material-ui/core';
 import { generateRandomColor } from 'utils';
 import clsx from 'clsx';
-import { ChatWindowStoreContext, ClassNameContext } from 'contexts';
 
 const useStyles = makeStyles((theme) => ({
-  avatar: (props) => ({
-    // @ts-ignore
-    color: theme.palette.getContrastText(props.avatarBg),
-    // @ts-ignore
-    backgroundColor: props.avatarBg,
+  // @ts-ignore
+  avatar: ({ avatarBg }) => ({
+    color: theme.palette.getContrastText(avatarBg),
+    backgroundColor: avatarBg,
   }),
 }));
 
@@ -32,10 +30,16 @@ ImageAvatar.defaultProps = {
 
 export const TextAvatar = (props) => {
   const { name, className } = props;
+  const isFirstLetterAlpha = name.charAt(0).match(/[a-zA-Z]/);
+
+  // Get first character & capitalize if it is alphabet
+  // otherwise get first unicode character
+  // https://stackoverflow.com/a/46159939/6842304
+  const txt = isFirstLetterAlpha ? name.charAt(0).toUpperCase() : [...name][0];
 
   return (
     <Avatar className={clsx(useStyles({ avatarBg: generateRandomColor(name) }).avatar, className)}>
-      {name ? name.charAt(0).toUpperCase() : '?'}
+      {txt}
     </Avatar>
   );
 };
@@ -50,25 +54,30 @@ TextAvatar.defaultProps = {
 };
 
 const CustomAvatar = (props) => {
-  const chatWindowStore = useContext(ChatWindowStoreContext);
-  const className = useContext(ClassNameContext);
-  if (chatWindowStore.avatarUrl) {
-    return (
-      <ImageAvatar
-        name={chatWindowStore.name}
-        avatarUrl={chatWindowStore.avatarUrl}
-        className={className}
-      />
-    );
+  const { name, avatarUrl, className } = props;
+  if (avatarUrl) {
+    return <ImageAvatar name={name} avatarUrl={avatarUrl} className={className} />;
   }
-  if (chatWindowStore.name) {
-    return <TextAvatar name={chatWindowStore.name} />;
+  if (name) {
+    return <TextAvatar name={name} className={className} />;
   }
   return (
-    <Avatar className={className}>
+    <Avatar>
       <Icon>person_search</Icon>
     </Avatar>
   );
+};
+
+CustomAvatar.propTypes = {
+  name: PropTypes.string,
+  avatarUrl: PropTypes.string,
+  className: PropTypes.string,
+};
+
+CustomAvatar.defaultProps = {
+  name: '',
+  avatarUrl: '',
+  className: '',
 };
 
 export default CustomAvatar;

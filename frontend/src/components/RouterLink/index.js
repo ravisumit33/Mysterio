121f51b535ac1055430d131581fc8926c35c7bb0
Sumit Kumar <ravi.sumit33@gmail.com>
2021-07-06 12:32:59 +0530

    Add custom router link component

---
+++ frontend/src/components/RouterLink/index.js
@@ -0,0 +1,8 @@
+import { Link } from 'react-router-dom';
+import { styled } from '@material-ui/core/styles';
+
+const RouterLink = styled(Link)({
+  textDecoration: 'none',
+});
+
+export default RouterLink;

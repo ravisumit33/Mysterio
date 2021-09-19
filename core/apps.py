851256d15987fa2591196fc1ccffe8f6263a5a5e
Sumit Kumar <ravi.sumit33@gmail.com>
2021-09-20 00:38:33 +0530

    Delete core django app

--- core/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class CoreConfig(AppConfig):
-    """Config for core django application"""
-
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "core"

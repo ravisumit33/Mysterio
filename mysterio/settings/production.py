4c2b65367f24e805921a4d9d168a743fdaf23ad9
Sumit Kumar <ravi.sumit33@gmail.com>
2022-02-12 03:17:01 +0530

    Revert "Use professional email (#293)" (#297)
    This reverts commit 32a8555746f739fbf6a890be693d98d339458591.

--- mysterio/settings/production.py
+++ mysterio/settings/production.py
@@ -49,9 +49,9 @@ SESSION_COOKIE_SAMESITE = "None"
 
 EMAIL_USE_TLS = True
 
-EMAIL_HOST = "smtpout.secureserver.net"
+EMAIL_HOST = "smtp.gmail.com"
 
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 

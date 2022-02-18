45fc0fc9c1f026fecafee780f11fc16a3d4ba315
Sumit Kumar <ravi.sumit33@gmail.com>
2022-02-18 13:04:31 +0530

    Add support for html serving on local

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
 import logging
 from django.views.generic.base import TemplateView
 from django.template.exceptions import TemplateDoesNotExist
-from django.http import Http404
+from django.conf import settings
+from django.http import Http404, HttpResponseRedirect
 from django.template.loader import get_template
 
 logger = logging.getLogger(__name__)
@@ -16,6 +17,9 @@ class FrontendView(TemplateView):
     template_name = "index.html"
 
     def get(self, request, *args, **kwargs):
+        if settings.DEBUG:
+            frontend_root_url = "http://localhost:3000"
+            return HttpResponseRedirect(frontend_root_url + request.path)
         try:
             get_template(self.template_name)
             return super().get(request, *args, **kwargs)

afbc3a99bf506c85898dd7de1f70ad906839280f
ravisumit33 <ravi.sumit33@gmail.com>
2022-05-14 17:46:32 +0530

    Fix db backend

--- mysterio/settings/production.py
+++ mysterio/settings/production.py
@@ -18,7 +18,7 @@ ALLOWED_HOSTS = [
 if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
